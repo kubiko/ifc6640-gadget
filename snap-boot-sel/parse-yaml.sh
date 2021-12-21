kubiko/if6640-gadget

snap-boot-sel: update env helper for uc20 lk environment
Signed-off-by: Ondrej Kubik <ondrej.kubik@canonical.com>

snap-boot-sel/parse-yaml.sh
@@ -45,6 +45,9 @@ get_part_names() {
     x=$((x+1))
   done
 }
-# volumes__structure_18_role
-echo -e $(get_part_names ${1} 'system-boot-image\|bootimg' '--boot-' ) \
-        $(get_part_names ${1} 'system-recovery-image' '--recovery-' ) \
+if [ "${2}" = "recovery" ]; then
+  echo -e $(get_part_names ${1} 'system-recovery-image' '--recovery-' )
+fi
+if [ "${2}" = "runtime" ]; then
+  echo -e $(get_part_names ${1} 'system-boot-image\|bootimg' '--boot-' )
+fi

kubiko/ifc6640-gadget

snap-boot-sel/parse-yaml.sh
@@ -45,6 +45,9 @@ get_part_names() {
     x=$((x+1))
   done
 }
-# volumes__structure_18_role
-echo -e $(get_part_names ${1} 'system-boot-image\|bootimg' '--boot-' ) \
-        $(get_part_names ${1} 'system-recovery-image' '--recovery-' ) \
+if [ "${2}" = "recovery" ]; then
+  echo -e $(get_part_names ${1} 'system-seed-image' '--recovery-' )
+fi
+if [ "${2}" = "runtime" ]; then
+  echo -e $(get_part_names ${1} 'system-boot-image\|bootimg' '--boot-' )
+fi